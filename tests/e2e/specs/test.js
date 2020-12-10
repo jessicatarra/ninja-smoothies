cd5f1c8b0d9375fbb43f1f8022686bbcaeea13bb
Jessica Tarra <jessictarra@gmail.com>
2020-12-10 18:59:23 -0300

    updating test.js

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,20 +1,28 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('My First Test', () => {
-  it('Editing Smoothie Ingredientes', () => {
-    cy.visit('/')
-    cy.get('.index > :nth-child(1) > .btn-floating').click()
-    cy.get('.add-ingredient > input').type('coffee')
-    cy.get('.add-ingredient > .material-icons').click()
-    cy.get('.add-ingredient > input').type('cinnamon')
-    cy.get('.add-ingredient > .material-icons').click()
-    cy.get('.add-ingredient > input').type('more coffee please')
-    cy.get('.add-ingredient > .material-icons').click()
-    cy.get(':nth-child(3) > .material-icons').click()
+  // it('Editing Smoothie Ingredientes', () => {
+  //   //visiting home page
+  //   cy.visit('/')
 
+  //   //adding ingredients
+  //   cy.get('.index > :nth-child(1) > .btn-floating').click()
+  //   cy.get('.add-ingredient > input').type('coffee')
+  //   cy.get('.add-ingredient > .material-icons').click()
+  //   cy.get('.add-ingredient > input').type('cinnamon')
+  //   cy.get('.add-ingredient > .material-icons').click()
+  //   cy.get('.add-ingredient > input').type('more coffee please')
+  //   cy.get('.add-ingredient > .material-icons').click()
 
-    cy.get('.btn').click()
-  });
+  //   //deling one ing
+  //   cy.get(':nth-child(3) > .material-icons').click()
+
+  //   //updating smoothie
+  //   cy.get('.btn').click()
+
+  //   //waiting to go to home page by identifing an element
+  //   cy.get(':nth-child(1) > .chip').should('be.visible')
+  // });
 
   it('Editing Smoothie Ingredientes #2', () => {
     cy.visit('/')
@@ -27,27 +35,32 @@ describe('My First Test', () => {
     cy.get('.add-ingredient > .material-icons').click()
     cy.get(':nth-child(3) > .material-icons').click()
 
-
     cy.get('.btn').click()
+      //waiting to go to home page by identifing an element
+    cy.get(':nth-child(1) > .chip').should('be.visible')
+  
   });
 
-  it('Adding Smoothie', () => {
-    cy.visit('/')
-    cy.get('.nav-content > .btn-floating > a > .material-icons').click()
-    cy.get('.title > input').type('Power Greens')
-    cy.get('.add-ingredient > input').type('Spinachs')
-    cy.get('.add-ingredient > .material-icons').click()
-    cy.get('.add-ingredient > input').type('bananas')
-    cy.get('.add-ingredient > .material-icons').click()
-    cy.get('.add-ingredient > input').type('almond milk')
-    cy.get('.add-ingredient > .material-icons').click()
+  // it('Adding Smoothie', () => {
+  //   cy.visit('/')
+  //   cy.get('.nav-content > .btn-floating > a > .material-icons').click()
+  //   cy.get('.title > input').type('Power Greens')
+  //   cy.get('.add-ingredient > input').type('Spinachs')
+  //   cy.get('.add-ingredient > .material-icons').click()
+  //   cy.get('.add-ingredient > input').type('bananas')
+  //   cy.get('.add-ingredient > .material-icons').click()
+  //   cy.get('.add-ingredient > input').type('almond milk')
+  //   cy.get('.add-ingredient > .material-icons').click()
 
 
-    cy.get('.btn').click()
-  });
+  //   cy.get('.btn').click()
+  //   //waiting to go to home page by identifing an element
+  //   cy.get(':nth-child(1) > .chip').should('be.visible')
+  
+  // });
 
-  it('Delete a Smoothie', () => {
-    cy.visit('/')
-    cy.get(':nth-child(2) > .card-content > .material-icons').click()
-  })
+  // it('Delete a Smoothie', () => {
+  //   cy.visit('/')
+  //   cy.get(':nth-child(2) > .card-content > .material-icons').click()
+  // })
 })
